ded642c502e4efa7cd0ad3d8f38821cced7c9ba9
umjiwoo <uts417923@gmail.com>
2024-08-08 23:48:05 +0900

    [BOJ] 14567 / 선수과목/ o

--- week1/umjiwoo/BOJ14567.java
+++ week1/umjiwoo/BOJ14567.java
@@ -5,7 +5,6 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class BOJ14567 {
-
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		String[] nm=sc.nextLine().split(" ");
