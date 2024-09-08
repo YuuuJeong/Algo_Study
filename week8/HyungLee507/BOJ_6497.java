6e69d6cd3a61c708ef0d89e97fa447d45e91d3f2
leedonghyun <skydh507@ajou.ac.kr>
2024-09-08 16:54:26 +0900

    [BOJ] Modify 6497 전력난 O

--- week8/HyungLee507/BOJ_6497.java
+++ week8/HyungLee507/BOJ_6497.java
@@ -1,91 +1,83 @@
-package backjoon;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+import java.util.stream.IntStream;
 
 public class Main {
-    static int N, L;
-    static int[][] map;
+    static int N, M;
+    static List<Integer> adjustCity;
+    static int[] parent;
+
+    private static class Road implements Comparable<Road> {
+        int start;
+        int end;
+        int cost;
+
+        public Road(int start, int end, int cost) {
+            this.start = start;
+            this.end = end;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Road o) {
+            return Integer.compare(this.cost, o.cost);
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        N = Integer.parseInt(st.nextToken());
-        L = Integer.parseInt(st.nextToken());
-        map = new int[N][N];
-        for (int i = 0; i < N; i++) {
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        while (true) {
             st = new StringTokenizer(bf.readLine());
-            for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        int answer = 0;
-
-        for (int i = 0; i < N; i++) {
-            int[] temp = new int[N];
-            for (int j = 0; j < N; j++) {
-                temp[j] = map[i][N - 1 - j];
-            }
-            if (isCorrect(map[i])) {
-                answer++;
-            }
-        }
-        for (int i = 0; i < N; i++) {
-            int[] temp = new int[N];
-            for (int j = 0; j < N; j++) {
-                temp[j] = map[j][i];
-            }
-            if (isCorrect(temp)) {
-                answer++;
-            }
-        }
-        System.out.println(answer);
-    }
-
-    private static boolean isCorrect(int[] arr) {
-        int temp;
-        int count = 1;
-        int index = 0;
-        while (index < N) {
-            temp = arr[index];
-            if (index + 1 >= N) {
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            if (M == 0 && N == 0) {
                 break;
             }
-            int next = arr[index + 1];
-
-            if (Math.abs(next - temp) >= 2) {
-                return false;
+            parent = IntStream.range(0, M).toArray();
+            PriorityQueue<Road> roads = new PriorityQueue<>();
+            long answer = 0;
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(bf.readLine());
+                int start = Integer.parseInt(st.nextToken());
+                int end = Integer.parseInt(st.nextToken());
+                int cost = Integer.parseInt(st.nextToken());
+                roads.add(new Road(start, end, cost));
+                answer += cost;
             }
-            if (next == temp) {
-                count++;
-                index++;
-                continue;
+            int count = 0;
+            while (count < M - 1) {
+                Road poll = roads.poll();
+                if (find(poll.start) != find(poll.end)) {
+                    union(poll.start, poll.end);
+                    count++;
+                    answer -= poll.cost;
+                }
             }
-            // 올라갔을 때
-            if (temp - next == -1) {
-                if (count >= L) {
-                    count = 1;
-                    index++;
-                } else {
-                    return false;
-                }
-            } else if (temp - next == 1) {
-                if (index + L >= N) {
-                    return false;
-                }
-                for (int i = index + 1; i < index + 1 + L; i++) {
-                    if (arr[i] != next) {
-                        return false;
-                    }
-                }
-                index += L;
-                count = 0;
-            }
-
+            sb.append(answer).append('\n');
         }
-        return true;
+        System.out.println(sb);
+        bf.close();
     }
 
+    private static void union(int x, int y) {
+        int parX = find(x);
+        int parY = find(y);
+        if (parX == parY) {
+            return;
+        }
+        parent[parX] = parY;
+    }
+
+    private static int find(int x) {
+        if (x == parent[x]) {
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
 }
