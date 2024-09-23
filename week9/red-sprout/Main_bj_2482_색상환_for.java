e6b18f71471596ecc327f0be790bad28aab5d9eb
Jaewan Joo <jjw05015@gmail.com>
2024-09-23 12:59:09 +0900

    [BOJ] 2482 / 색상환 / O
    dp - for문 활용(수정)

--- week9/red-sprout/Main_bj_2482_색상환_for.java
+++ week9/red-sprout/Main_bj_2482_색상환_for.java
@@ -8,14 +8,14 @@ public class Main_bj_2482_색상환_for {
 		int MAX = 1_000_000_003;
 		int[][] dp = new int[N + 1][N + 1];
 		for(int i = 1; i <= N; i++){
-        dp[i][0] = 1;
-        dp[i][1] = i;
-    }
+			dp[i][0] = 1;
+			dp[i][1] = i;
+		}
 		for(int i = 3; i <= N; i++){
-        for(int j = 2; j <= (i + 1) / 2; j++){
-            dp[i][j] = (dp[i - 1][j] + dp[i - 2][j - 1]) % MAX;
-        }
-    }
+			for(int j = 2; j <= (i + 1) / 2; j++){
+				dp[i][j] = (dp[i - 1][j] + dp[i - 2][j - 1]) % MAX;
+			}
+		}
 		System.out.println((dp[N - 3][K - 1] + dp[N - 1][K]) % MAX);
 		br.close();
 	}
