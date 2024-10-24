12ec7e54be37369f35423804409d2661ce85529b
spiky-haired-Pyramid-head <qns0147@gmail.com>
2024-10-24 16:12:43 +0900

    [BOJ] 8980 / 택배 / O

--- week12/tinyfrogs/BOJ_8980.java
+++ week12/tinyfrogs/BOJ_8980.java
@@ -3,13 +3,12 @@ import java.util.*;
 
 public class BOJ_8980 {
 
-    static int N, C, M;
+    static int N, C, M, result;
+
     static Point[] points;
 
     static class Point {
-        int from;
-        int to;
-        int value;
+        int from, to, value;
 
         public Point(int from, int to, int value) {
             this.from = from;
@@ -22,49 +21,45 @@ public class BOJ_8980 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
+        result = 0;
         N = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(br.readLine());
         points = new Point[M];
-
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             points[i] = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
         }
 
-        //목적지가 빠른(가까운) 곳으로 -> 같으면 출발이 더 빠른 곳으로
-        Arrays.sort(points, (o1, o2) -> {
-            if (o1.to == o2.to) return o1.from - o2.from;
-            return o1.to - o2.to;
-        });
 
-//        for(Point p : points)
-//            System.out.println(p.from + " " + p.to + " " + p.value);
-
-        //저장할 수 있는 공간 주기
+        Arrays.sort(points, (o1, o2) -> o1.to - o2.to);
         int[] homeValues = new int[N + 1];
-        for (int i = 1; i <= N; i++) homeValues[i] = C;
+        for (int i = 0; i <= N; i++) homeValues[i] = C;
 
-        int ans = 0;
+        findHomeValues(homeValues);
+        System.out.println(result);
+        br.close();
+    }
 
-        //택배 경로 반복
-        for (int i = 0; i < homeValues.length; i++) {
+    static void findHomeValues(int[] homeValues) {
+        for (int i = 0; i < M; i++) {
             int start = points[i].from;
             int end = points[i].to;
-            int box = points[i].value;
-            int recentValue = Integer.MAX_VALUE;
+            int value = points[i].value;
 
-            for (int j = start; j < end; j++) {
-                recentValue = Math.min(recentValue, homeValues[j]);
+            int min = Integer.MAX_VALUE;
+
+            for (int s = start; s < end; s++) {
+                min = Math.min(min, homeValues[s]);
             }
 
-            for (int j = start; j < end; j++) {
-                homeValues[j] -= Math.min(recentValue, box);
+            value = Math.min(value, min);
+            result += value;
+
+            for (int s = start; s < end; s++) {
+                homeValues[s] -= value;
             }
-            ans += Math.min(recentValue, box);
         }
-
-        System.out.println(ans);
     }
 }
